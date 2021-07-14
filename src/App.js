edad34b5d4019de53710b49a9603a19482d50c6e
rohitrkx <rohitsinghx677@gmail.com>
2021-07-14 10:46:19 +0530

    added react helloworld

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./style.css"
+import Button from "./Button.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+    return(
+            <div className="container">
+                <h1 style={{textAlign:"Center",}}><span>H</span>ello World</h1>
+                <p><span className="rspan">R</span>eact is a JavaScript library for building user interfaces.</p>
+                <Button buttonText = "Declarative"/>
+                <Button buttonText = "Component Based"/>
+                <Button buttonText = "Learn Once, Write Anywhere"/>
+            </div>
+    )
 }
 
-export default App;
+export default App
